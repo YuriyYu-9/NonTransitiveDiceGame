0275769955c64a3e732cd67f5173bc387d866ef9
Yuriy_Yu <ugajurij123@gmail.com>
2025-01-30 23:05:13 +0500

    Fixed DiceParser validation

--- NonTransitiveDiceGame/DiceParser.cs
+++ NonTransitiveDiceGame/DiceParser.cs
@@ -1,36 +1,29 @@
-﻿using System;
+﻿using NonTransitiveDiceGame;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace NonTransitiveDiceGame
+public static class DiceParser
 {
-    internal class DiceParser
+    public static List<Dice> Parse(string[] args)
     {
-        public static List<Dice> Parse(string[] args)
+        if (args.Length < 3)
+            throw new ArgumentException("Please specify at least three dice.");
+
+        List<Dice> diceSet = new List<Dice>();
+
+        foreach (var arg in args)
         {
-            if (args.Length < 3)
-            {
-                throw new ArgumentException("You must specify at least 3 dice configurations.");
-            }
+            int[] faces = arg.Split(',')
+                             .Select(f => int.TryParse(f, out int num) ? num : throw new ArgumentException($"Invalid value '{f}'. Only integers are allowed."))
+                             .ToArray();
 
-            List<Dice> diceList = new List<Dice>();
+            if (faces.Length != 6)
+                throw new ArgumentException($"Invalid dice configuration: {arg}. Each dice must have exactly 6 faces.");
 
-            foreach (var arg in args)
-            {
-                try
-                {
-                    int[] faces = arg.Split(',').Select(int.Parse).ToArray();
-                    diceList.Add(new Dice(faces));
-                }
-                catch
-                {
-                    throw new ArgumentException($"Invalid dice format: {arg}. Example: 2,3,4,5,6,7");
-                }
-            }
-
-            return diceList;
+            diceSet.Add(new Dice(faces));
         }
+
+        return diceSet;
     }
 }
